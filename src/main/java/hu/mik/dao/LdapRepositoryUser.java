b9c7f36580b7180699a9896381f5a58f5c4e0f87
Holler Soma <holler.soma@freemail.hu>
2018-07-15 20:41:13 +0200

    Refact

--- src/main/java/hu/mik/dao/LdapRepositoryUser.java
+++ src/main/java/hu/mik/dao/LdapRepositoryUser.java
@@ -2,17 +2,17 @@ package hu.mik.dao;
 
 import java.util.List;
 
-import javax.naming.Name;
-
 import org.springframework.stereotype.Repository;
 
-import hu.mik.beans.LdapGroup;
 import hu.mik.beans.LdapUser;
 
 @Repository
-public interface LdapRepositoryUser extends org.springframework.data.ldap.repository.LdapRepository<LdapUser>{
+public interface LdapRepositoryUser extends org.springframework.data.ldap.repository.LdapRepository<LdapUser> {
 	LdapUser findByUsername(String username);
+
 	List<LdapUser> findByFullNameContaining(String fullName);
+
+	@Override
 	List<LdapUser> findAll();
 //	LdapUser findById(Name name);
 }
