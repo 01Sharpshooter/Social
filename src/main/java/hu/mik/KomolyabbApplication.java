5e60db37783ba0f4507a5293f30104d9af272949
Holler Soma <holler.soma@freemail.hu>
2018-11-06 18:52:27 +0100

    Removed unnecessary annotation

--- src/main/java/hu/mik/KomolyabbApplication.java
+++ src/main/java/hu/mik/KomolyabbApplication.java
@@ -1,12 +1,10 @@
 package hu.mik;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
 @ServletComponentScan
 @ComponentScan
 @SpringBootApplication
